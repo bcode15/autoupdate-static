7154476f636cbd1dc29231445d371bd381c79381
Bruce Johnson <brucejo.wk@outlook.com>
2022-04-27 09:43:17 -0700

    RL:brucejo:autoupdate-static@0.1.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'brucejo:autoupdate-static',
-  version: '0.1.0',
+  version: '0.1.1',
   // Brief, one-line summary of the package.
   summary: 'Enables autoupdate for staticly served websites',
   // URL to the Git repository containing the source code for this package.
